1b4957ac88eae2c5fc00972b090eae9faeb1b80f
Kesavan Rangan <68580746+kesavan-rangan@users.noreply.github.com>
2022-03-27 11:59:33 +0530

    Installing sudo

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,5 @@
+whoami
+apt-get install -y sudo
 sudo apt-get update && sudo apt-get install -y curl && sudo apt-get install -y lsb-release
 export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
 echo "deb http://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
